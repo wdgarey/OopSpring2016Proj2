34aa1758577bf7bc48cae5d932770326e3dc1059
wdgarey <whitewez@hotmail.com>
2016-04-16 08:37:47 -0400

    First Commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * Author: Wesley Garey
+ * Date: April 14, 2016
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * The entry point of the program.
+ * @return The exit code.
+ */
+int main()
+{   
+    return EXIT_SUCCESS;
+}
+
